edb0f909d29eb69c7959816a6f50cf361dbd4faf
MironProFit <mpf_3d@vk.com>
2025-10-22 19:37:43 +0500

    добавляет кнопку и  функционал выбора периода

--- src/pages/PeriodPage/PeriodPage.jsx
+++ src/pages/PeriodPage/PeriodPage.jsx
@@ -1,13 +1,28 @@
+import { useNavigate } from 'react-router-dom'
 import CalendarComponent from '../../components/Calendar/Calendar'
 import { useAppCoontext } from '../../contexts/AppContext'
-import { Container, ContainerGroup, PageTitle } from '../../styles/GlobalStyled'
+import { Container, ContainerGroup, FlexContainer, PageTitle } from '../../styles/GlobalStyled'
+import { AddPeriod } from '../../components/Expenses/PeriodExpenses.styles'
 
 function PeriodPage() {
     const { isMobile } = useAppCoontext()
+    const navigate = useNavigate()
+
+    const handleConfirmSelectedPeriod = () => {
+        console.log('кнопка нажата')
+        alert('Выбор подтвержден')
+        navigate('/analysis')
+    }
+
     return (
         <>
             <Container $isMobile={isMobile}>
                 <CalendarComponent />
+                {isMobile && (
+                    <FlexContainer>
+                        <AddPeriod onClick={handleConfirmSelectedPeriod}>Выбрать другой период</AddPeriod>
+                    </FlexContainer>
+                )}
             </Container>
         </>
     )
